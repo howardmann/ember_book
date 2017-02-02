8493ffdc919eea18382a8772d9f328ca4d97cecb
howardmann <howardmann27@gmail.com>
2017-02-02 13:47:24 +1100

    Add delete publisher feature

--- app/components/edit-publisher.js
+++ app/components/edit-publisher.js
@@ -6,6 +6,15 @@ export default Ember.Component.extend({
       this.get('model').save().then(() => {
         this.get('router').transitionTo('publishers');
       });
+    },
+    delete(){
+      if (confirm('Are you sure?')){
+        this.get('model').destroyRecord().then(()=>{
+          this.get('router').transitionTo('publishers');
+        });
+      } else {
+        return ;
+      }
     }
   }
 });
